3db3a425e48d3b0028a20639cee7ce2f3c7c2c68
andresrestrepo <andresr.alz@gmail.com>
2024-03-14 10:24:14 -0500

    bugs fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-    base: "/portfolio_team",
-    build: {
-        assets: "assets"
-    }
+  base: "/portfolio_team",
+  build: {
+    assets: "assets"
+  },
+  integrations: [mdx()]
 });
